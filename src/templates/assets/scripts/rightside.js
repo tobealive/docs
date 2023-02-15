b92c79f07d115d31b4cf5caa8fb7e0fedb096d76
i582 <51853996+i582@users.noreply.github.com>
2023-02-15 14:48:37 +0400

    added right menu changes when scroll

---
+++ src/templates/assets/scripts/rightside.js
@@ -0,0 +1,54 @@
+document.addEventListener('DOMContentLoaded', () => {
+    setupRightSideWatcher()
+});
+
+function setupRightSideWatcher() {
+    const setActive = (section) => {
+        const link = document.querySelector('.aside-container li a[href="#' + section + '"]');
+        if (link) {
+            const parentLi = link.parentElement;
+            parentLi.classList.add('active');
+        }
+    }
+
+    const sectionPositions = [];
+    const sections = document.querySelectorAll('h2');
+
+    if (sections.length === 0) {
+        return;
+    }
+
+    sections.forEach(section => {
+        sectionPositions.push(section.offsetTop);
+        console.log(section.offsetTop)
+    });
+
+    window.addEventListener('scroll', function (_) {
+        document.querySelectorAll('.aside-container li[class="active"]').forEach(link => {
+            link.classList.remove('active');
+        });
+
+        const scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+
+        const firstSectionPosition = sectionPositions[0] ?? 0;
+        if (scrollPosition < firstSectionPosition) {
+            setActive(sections[0].id);
+            return
+        }
+
+        const lastSectionPosition = sectionPositions[sectionPositions.length - 1] ?? 0;
+        if (scrollPosition > lastSectionPosition) {
+            setActive(sections[sections.length - 1].id);
+            return
+        }
+
+        for (let i = sectionPositions.length - 1; i >= 0; i--) {
+            const section = sections[i];
+            const position = sectionPositions[i];
+            if (scrollPosition + 200 >= position) {
+                setActive(section.id);
+                break;
+            }
+        }
+    });
+}
