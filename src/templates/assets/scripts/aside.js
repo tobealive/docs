2751e25befa5a73b353d0c5736a2b314b1dfa98a
i582 <51853996+i582@users.noreply.github.com>
2023-02-15 13:12:09 +0400

    added initial tree view in left sidebar

---
+++ src/templates/assets/scripts/aside.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const activeLink = document.querySelector(".link.active");
+    if (activeLink !== null) {
+        activeLink.scrollIntoView({block: "center", behavior: "auto"});
+
+        let parentItem = activeLink.closest('.js-item');
+        // skip first item
+        parentItem = parentItem.parentElement.closest('.js-item');
+        for (let i = 0; i < 10; i++) {
+            if (parentItem) {
+                parentItem.classList.add('open');
+                const list = parentItem.querySelector('.js-sub-list');
+                if (list) {
+                    list.classList.add('open');
+                }
+                parentItem = parentItem.parentElement.closest('.js-item');
+            }
+
+            if (!parentItem) {
+                break;
+            }
+        }
+    }
+})
+
+document.querySelectorAll('.js-item .link').forEach((item) => {
+    item.addEventListener('click', () => {
+        const parent = item.parentElement;
+        const innerList = parent.querySelector('.js-sub-list');
+        if (innerList) {
+            innerList.classList.toggle('open');
+            parent.classList.toggle('open');
+        }
+    })
+});
+
+document.querySelectorAll('.js-sub-list').forEach((subList) => {
+    subList.classList.remove('open')
+});
