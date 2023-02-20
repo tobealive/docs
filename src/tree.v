49796a61d543d1dfa382d6cd3491e1045168e6c8
walkingdevel <mosquito.is.dead@gmail.com>
2023-02-20 16:11:37 +0000

    wip: last updated

--- src/tree.v
+++ src/tree.v
@@ -2,18 +2,20 @@ module main
 
 import os
 import json
+import time
 
 [heap]
 struct Node {
 	title string
 	url   string
 mut:
-	html_url      string
-	markdown_path string
-	contents      []Node
-	body          string
-	is_folder     bool
-	parent        &Node = unsafe { nil }
+	html_url          string
+	markdown_path     string
+	contents          []Node
+	body              string
+	modification_time time.Time
+	is_folder         bool
+	parent            &Node = unsafe { nil }
 }
 
 fn build_docs_tree(path string) !&Node {
@@ -27,16 +29,17 @@ fn build_docs_tree(path string) !&Node {
 		if is_markdown_url {
 			markdown_path := os.join_path(path, child.url)
 			markdown_content := os.read_file(markdown_path)!
-
 			html_filename := '${title_to_filename(child.title)}.html'
 			directory_name := title_to_filename(child.parent.title)
-			child.html_url = '${directory_name}/${html_filename}'
-			child.markdown_path = markdown_path
+			modification_time := get_last_modification_date_of_file(markdown_path)
 
 			mut transformer := MarkdownTransformer{
 				content: markdown_content
 			}
 
+			child.modification_time = time.unix(modification_time)
+			child.html_url = '${directory_name}/${html_filename}'
+			child.markdown_path = markdown_path
 			child.body = transformer.process()
 			continue
 		}
@@ -57,4 +60,14 @@ fn read_node_config(path string) !Node {
 
 fn is_markdown_file(path string) bool {
 	return path.ends_with('.md')
+}
+
+fn get_last_modification_date_of_file(path string) int {
+	result := os.execute('git log -1 --pretty="format:%ct" ${path}')
+
+	if result.exit_code != 0 {
+		panic(result.output)
+	}
+
+	return result.output.int()
 }
