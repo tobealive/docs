3591d094ece7f2996fd77e4c9e8a924dd2f6f869
i582 <51853996+i582@users.noreply.github.com>
2023-04-05 10:10:11 +0400

    minor updates

--- docs/main.md
+++ docs/main.md
@@ -1,3 +1,3 @@
-# V Language Documentation
+# V Language Documentation (aka Vlang Docs)
 
 See [main-page.html](/src/templates/index-page.html) for the source code of this page.
--- src/templates/assets/scripts/theme.js
+++ src/templates/assets/scripts/theme.js
@@ -1,6 +1,18 @@
 const playgrounds = [];
 
+// quick path to stop blinking the page
+const preferDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+const defaultTheme = preferDark ? "dark" : "light";
+
+const storedTheme = localStorage.getItem("theme");
+// if not stored, use default
+const currentTheme = storedTheme || defaultTheme;
+
+localStorage.setItem("theme", currentTheme);
+document.documentElement.setAttribute("data-theme", currentTheme);
+
 document.addEventListener('DOMContentLoaded', () => {
+    // do all changes after DOM is loaded
     const theme = localStorage.getItem("theme");
     if (theme) {
         setTheme(theme);
@@ -42,4 +54,20 @@ function setTheme(newTheme) {
             playground.setTheme(newTheme);
         });
     }
+
+    changeGiscusTheme(newTheme);
+}
+
+function changeGiscusTheme(theme) {
+    function sendMessage(message) {
+        const iframe = document.querySelector('iframe.giscus-frame');
+        if (!iframe) return;
+        iframe.contentWindow.postMessage({giscus: message}, 'https://giscus.app');
+    }
+
+    sendMessage({
+        setConfig: {
+            theme: theme
+        }
+    });
 }
