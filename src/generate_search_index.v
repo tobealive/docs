1ed630696011a4ce272739509b50cc7928a998e2
i582 <51853996+i582@users.noreply.github.com>
2023-04-07 12:01:31 +0400

    improved search index generator

---
+++ src/generate_search_index.v
@@ -0,0 +1,92 @@
+module main
+
+import strings
+import os
+import json
+
+fn generate_search_index() ! {
+	mut gen := SearchIndexGenerator{}
+	result := gen.generate()
+
+	os.write_file('docs_index.json', result)!
+}
+
+struct SearchIndexEntry {
+	id     int
+	parent string [omitempty]
+	title  string
+	body   string
+	url    string
+}
+
+struct SearchIndexGenerator {
+mut:
+	counter int
+	entries []SearchIndexEntry
+}
+
+fn (mut s SearchIndexGenerator) generate() string {
+	mut result := strings.new_builder(100)
+
+	os.walk(docs_path, fn [mut s] (file string) {
+		s.process_file(file)
+	})
+
+	result.write_string('[\n')
+
+	for index, entry in s.entries {
+		result.write_string(json.encode(entry))
+		if index < s.entries.len - 1 {
+			result.write_string(',\n')
+		}
+	}
+
+	result.write_string('\n')
+	result.write_string(']\n')
+	return result.str()
+}
+
+fn (mut s SearchIndexGenerator) process_file(filepath string) {
+	if !filepath.ends_with('.md') {
+		return
+	}
+	content := os.read_file(filepath) or { return }
+
+	lines := content.split_into_lines()
+	title := lines.first().trim(' #')
+
+	markdown_subtopics := split_source_by_topics(content, 2)
+	subtopics := extract_topics_from_markdown_parts(markdown_subtopics, true)
+
+	body := lines[1..].join('\n')
+
+	normalized_filename := filepath.replace_once('${docs_path}/', '').replace('.md', '.html')
+	html_path := 'https://docs.vlang.foundation/${normalized_filename}'
+
+	s.entries << SearchIndexEntry{
+		id: s.counter++
+		title: title
+		body: process_content(body)
+		url: html_path
+	}
+
+	for subtopic in subtopics {
+		subtopic_content := subtopic.markdown_content.split_into_lines()[1..].join('\n')
+
+		s.entries << SearchIndexEntry{
+			id: s.counter++
+			parent: title
+			title: subtopic.title
+			body: process_content(subtopic_content)
+			url: html_path + '#' + subtopic.id
+		}
+	}
+}
+
+fn process_content(content string) string {
+	return content
+		.replace('\n', ' ')
+		.replace('\\', '\\\\')
+		.replace('\u0009', '')
+		.replace('"', '\\"')
+}
--- src/main.v
+++ src/main.v
@@ -1,8 +1,5 @@
 module main
 
-import os
-import strings
-
 const (
 	docs_path   = 'docs'
 	output_path = 'output'
@@ -23,59 +20,5 @@ fn main() {
 	copy_assets_to_output()!
 	copy_images_to_output()!
 
-	mut result := strings.new_builder(100)
-	mut builder := &result
-
-	result.write_string('[\n')
-
-	mut id := 0
-	mut id_ptr := &id
-	os.walk(docs_path, fn [mut builder, mut id_ptr] (file string) {
-		line := generate_json_line(file, mut id_ptr) or { return }
-		builder.write_string(line)
-	})
-
-	result.cut_last(2) // last comma
-	result.write_string('\n')
-	result.write_string(']\n')
-
-	os.write_file('docs_index.json', result.str())!
-}
-
-fn generate_json_line(filepath string, mut id_ptr &int) ?string {
-	if !filepath.ends_with('.md') {
-		return none
-	}
-	content := os.read_file(filepath) or { return '' }
-
-	lines := content.split_into_lines()
-	title := lines.first().trim(' #')
-
-	markdown_subtopics := split_source_by_topics(content, 2)
-	subtopics := extract_topics_from_markdown_parts(markdown_subtopics, true)
-
-	mut result := strings.new_builder(100)
-	body := lines[1..].join('\n')
-	(*id_ptr)++
-
-	html_path := 'https://docs.vlang.foundation/' +
-		filepath.replace_once('${docs_path}/', '').replace('.md', '.html')
-	result.write_string('{ "id": ${*id_ptr}, "title": "${title}", "body": "${process_content(body)}", "url": "${html_path}" },\n')
-
-	for subtopic in subtopics {
-		(*id_ptr)++
-
-		subtopic_content := subtopic.markdown_content.split_into_lines()[1..].join('\n')
-		result.write_string('{ "id": ${*id_ptr}, "parent": "${title}", "title": "${subtopic.title}", "body": "${process_content(subtopic_content)}", "url": "${html_path}#${subtopic.id}" },\n')
-	}
-
-	return result.str()
-}
-
-fn process_content(content string) string {
-	return content
-		.replace('\n', ' ')
-		.replace('\\', '\\\\')
-		.replace('\u0009', '')
-		.replace('"', '\\"')
+	generate_search_index()!
 }
